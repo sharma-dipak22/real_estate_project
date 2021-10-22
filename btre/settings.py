61f2f595bc96472b30718766200c3cb58a607ff0
sharma-dipak22 <dbsharma22897@gmail.com>
2021-10-22 21:36:01 +0530

    settings updated

--- btre/settings.py
+++ btre/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'btre/static')
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
